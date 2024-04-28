cceea01668a7c626317fc16a2459dd7cd70bf082
PedroRomeraDev <168337717+PedroRomeraDev@users.noreply.github.com>
2024-04-28 15:21:46 +0200

    calculator

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
-window.alert("Hello World!");
+function appendToDisplay(value) {
+    console.log('appendToDisplay:', value);
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    console.log('clearDisplay');
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    console.log('calculate');
+    try {
+        var expression = document.getElementById('display').value;
+        var result = Function('"use strict";return (' + expression + ')')();
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
